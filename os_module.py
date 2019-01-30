4c1b1621463d2aff0ca001c72fdb81708c40b38c
RishabhRawatAndroid <rishicentury1996@gmail.com>
2019-01-31 02:13:36 +0530

    Adding the OS module

---
+++ os_module.py
@@ -0,0 +1,11 @@
+import os
+
+print("Current DIR ", os.getcwd())  # get current working directory
+print("List DIR", os.listdir())  # show all folder and file current directory
+# os.makedir('file path') #make a new directory
+# os.makedirs('folder/folder/file')
+# os.rmdir('file path') #delete diectory
+# os.removedirs('filepath/filepath') # delete deep level directory
+#os.rename("actual file",'rename file')
+
+print("Main Python file ", os.stat('main.py'))  # print the file meta data
